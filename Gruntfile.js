58c3daa140350a28d57492c432893e1a6a1d4233
chris <kordex0@gmail.com>
2015-04-19 15:16:55 -0700

    fixed css compression

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,9 @@ module.exports = function(grunt) {
       },
     },
     cssmin: {
+      options: {
+        processImport: false
+      },
       build: {
         files: [{
           expand: true,
