795553ad9f68962d83ebae4aa6bc69e8ad40c6ac
UvicDanceAdmin <ballroom@uvicdance.com>
2015-07-15 18:47:17 -0700

    deleted Ruth folder from images folder

--- images/ruth_files/XZ2-FjjwKeI.js
+++
@@ -1,6 +0,0 @@
-/*1374668158,178146589*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["Zkght"]); }
-
-__d("legacy:photos-constants",["PhotosConst"],function(a,b,c,d){a.PhotosConst=b('PhotosConst');},3);
-__d("legacy:PhotoTagger",["PhotoTagger"],function(a,b,c,d){a.PhotoTagger=b('PhotoTagger');},3);
